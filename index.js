29cebddd07df8254bf0406f6bf00b73adf89d8d9
Eric McCarthy <eric@limulus.net>
2014-08-07 14:54:47 -0400

    Refactored SVG playground stuff into its own class.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var JavaScriptPlayground = require("./lib/JavaScriptPlayground.js")
-var SVGPlaygroundJSTemplate = require("./lib/SVGPlaygroundJSTemplate.js")
-var svgCreateElement = require("svg-create-element")
+"use strict"
+
+var SVGPlayground = require("./lib/SVGPlayground.js")
 
 // Async initialization
 if (document.readyState === "loading") {
@@ -11,32 +11,8 @@ else {
 }
 
 function initializePlayground () {
-    var svgContainerElem = document.createElement("div")
-    svgContainerElem.style.width = "50%"
-    svgContainerElem.style.height = "400px"
-    svgContainerElem.style.float = "left"
-    document.body.appendChild(svgContainerElem)
-
-    var playgroundElem = document.createElement("div")
-    playgroundElem.style.width = "50%"
-    playgroundElem.style.height = "400px"
-    playgroundElem.style.float = "left"
-    document.body.appendChild(playgroundElem)
-
-    var generateUserCodeArguments = function () {
-        var svgRoot = svgCreateElement("svg")
-        removeAllChildNodes(svgContainerElem)
-        svgContainerElem.appendChild(svgRoot)
-        return [svgRoot]
-    }
-
-    var jsTemplate = new SVGPlaygroundJSTemplate()
-    new JavaScriptPlayground(playgroundElem, jsTemplate, generateUserCodeArguments)
-}
-
-function removeAllChildNodes (element) {
-    while (element.lastChild) {
-        element.removeChild(element.lastChild)
-    }
+    var playgroundContainerElem = document.createElement("div")
+    document.body.appendChild(playgroundContainerElem)
+    new SVGPlayground(playgroundContainerElem)
 }
 
