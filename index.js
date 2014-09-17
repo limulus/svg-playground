7b1f3c4f90d045b1da6b82b9e45c03db7a170ed1
Eric McCarthy <eric@limulus.net>
2014-09-16 22:53:05 -0700

    http-file-share integration.

--- index.js
+++ index.js
@@ -3,16 +3,30 @@
 "use strict"
 
 var http = require("http")
+var path = require("path")
 var beefy = require("beefy")
+var httpFileShare = require("http-file-share")
 var portfinder = require("portfinder")
+
+var projectDir = process.cwd()
 
 portfinder.getPort(function (err, port) {
     if (err) throw err
 
-    var server = http.createServer(beefy({
+    var beefyHandler = beefy({
         "entries": ["./client.js"],
         "cwd": __dirname
-    }))
+    })
+
+    var fileShareHandler = httpFileShare(projectDir, /^\/files/)
+    var server = http.createServer(function (req, res) {
+        if (req.url.match(/^\/files/)) {
+            fileShareHandler(req, res)
+        }
+        else {
+            beefyHandler(req, res)
+        }
+    })
 
     server.listen(port, "127.0.0.1")
     console.log("Running application on http://127.0.0.1:"+port+"/")
