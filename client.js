a15486327c59f1f4e6c01d6ec5b3b67d37a224c6
Eric McCarthy <eric@limulus.net>
2014-09-18 14:14:21 -0700

    Added xhr module and updated version number.

--- client.js
+++ client.js
@@ -2,6 +2,8 @@
 
 var SVGPlayground = require("./lib/SVGPlayground.js")
 
+var xhr = require("xhr")
+
 // Async initialization
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", initializePlayground, false)
@@ -11,9 +13,17 @@ else {
 }
 
 function initializePlayground () {
-  var playgroundContainerElem = document.createElement("div")
-  playgroundContainerElem.style.height = "500px"
-  playgroundContainerElem.style.width = "100%"
-  document.body.appendChild(playgroundContainerElem)
-  new SVGPlayground(playgroundContainerElem)
+
+  xhr({"uri": "/svg-project.json"}, function (err, res, body) {
+    if (err) {
+      window.alert("Failed to load /svg-project.json")
+      return
+    }
+
+    var playgroundContainerElem = document.createElement("div")
+    playgroundContainerElem.style.height = "500px"
+    playgroundContainerElem.style.width = "100%"
+    document.body.appendChild(playgroundContainerElem)
+    new SVGPlayground(playgroundContainerElem)
+  })
 }
